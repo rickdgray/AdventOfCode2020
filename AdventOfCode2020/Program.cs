88ce724c08075bde9e232c6677b4fb6bed43d056
Rick Gray <rickdgray@outlook.com>
2020-12-12 00:17:33 -0600

    part2

--- AdventOfCode2020/Program.cs
+++ AdventOfCode2020/Program.cs
@@ -21,7 +21,7 @@ namespace AdventOfCode2020
             }
 
             //currently working on:
-            Day12.Part1(data);
+            Day13.Part1(data);
         }
     }
 
@@ -448,44 +448,141 @@ namespace AdventOfCode2020
             foreach (var line in data)
             {
                 var instruction = line.Substring(0, 1);
-                var distance = line.Substring(1);
+                var distance = int.Parse(line.Substring(1));
 
                 switch(instruction)
                 {
                     case "N":
-                        y += int.Parse(distance);
+                        y += distance;
                         break;
                     case "S":
-                        y -= int.Parse(distance);
+                        y -= distance;
                         break;
                     case "E":
-                        x += int.Parse(distance);
+                        x += distance;
                         break;
                     case "W":
-                        x -= int.Parse(distance);
+                        x -= distance;
                         break;
                     case "L":
-                        radians += int.Parse(distance) * Math.PI / 180;
+                        radians += distance * Math.PI / 180;
                         break;
                     case "R":
-                        radians -= int.Parse(distance) * Math.PI / 180;
+                        radians -= distance * Math.PI / 180;
                         break;
                     case "F":
-                        x += int.Parse(distance) * Math.Cos(radians);
-                        y += int.Parse(distance) * Math.Sin(radians);
+                        x += distance * Math.Cos(radians);
+                        y += distance * Math.Sin(radians);
                         break;
                 }
             }
 
             Console.WriteLine(Math.Abs(x) + Math.Abs(y));
         }
+
+        public static void Part2(List<string> data)
+        {
+            double waypointX = 10;
+            double waypointY = 1;
+
+            double shipX = 0;
+            double shipY = 0;
+
+            foreach (var line in data)
+            {
+                var instruction = line.Substring(0, 1);
+                var distance = int.Parse(line.Substring(1));
+
+                switch (instruction)
+                {
+                    case "N":
+                        waypointY += distance;
+                        break;
+                    case "S":
+                        waypointY -= distance;
+                        break;
+                    case "E":
+                        waypointX += distance;
+                        break;
+                    case "W":
+                        waypointX -= distance;
+                        break;
+                    case "L":
+                        {
+                            switch (distance)
+                            {
+                                case 90:
+                                    {
+                                        var tmp = waypointX;
+                                        waypointX = -1 * waypointY;
+                                        waypointY = tmp;
+                                        break;
+                                    }
+                                case 180:
+                                    {
+                                        waypointX = -1 * waypointX;
+                                        waypointY = -1 * waypointY;
+                                        break;
+                                    }
+                                case 270:
+                                    {
+                                        var tmp = waypointX;
+                                        waypointX = waypointY;
+                                        waypointY = -1 * tmp;
+                                        break;
+                                    }
+                                default:
+                                    throw new ArgumentException();
+                            }
+                            break;
+                        }
+                    case "R":
+                        {
+                            switch (distance)
+                            {
+                                case 90:
+                                    {
+                                        var tmp = waypointX;
+                                        waypointX = waypointY;
+                                        waypointY = -1 * tmp;
+                                        break;
+                                    }
+                                case 180:
+                                    {
+                                        waypointX = -1 * waypointX;
+                                        waypointY = -1 * waypointY;
+                                        break;
+                                    }
+                                case 270:
+                                    {
+                                        var tmp = waypointX;
+                                        waypointX = -1 * waypointY;
+                                        waypointY = tmp;
+                                        break;
+                                    }
+                                default:
+                                    throw new ArgumentException();
+                            }
+                            break;
+                        }
+                    case "F":
+                        {
+                            shipX += distance * waypointX;
+                            shipY += distance * waypointY;
+                            break;
+                        }
+                }
+            }
+
+            Console.WriteLine(Math.Abs(shipX) + Math.Abs(shipY));
+        }
     }
 
     public class Day13
     {
-        public static void Part1()
+        public static void Part1(List<string> data)
         {
-            throw new NotImplementedException();
+
         }
     }
 
