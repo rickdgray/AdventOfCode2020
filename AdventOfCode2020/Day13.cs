0b986b4a3dc1ec0c475cb6435a8388c1ac3efe47
Rick Gray <rickdgray@outlook.com>
2020-12-15 23:06:12 -0600

    day 13 part 1

--- AdventOfCode2020/Day13.cs
+++ AdventOfCode2020/Day13.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace AdventOfCode2020
 {
@@ -7,7 +8,30 @@ namespace AdventOfCode2020
     {
         public static long Part1(List<string> data)
         {
-            throw new NotImplementedException();
+            var earliestDepart = int.Parse(data.First());
+            var buses = data[1]
+                .Split(",")
+                .Where(n => !n.Equals("x"))
+                .Select(n => int.Parse(n))
+                .ToList();
+
+            var earliestBusTimes = new List<int>();
+            foreach (var bus in buses)
+            {
+                earliestBusTimes.Add(bus - (earliestDepart % bus) + earliestDepart);
+            }
+
+            var earliestBus = earliestBusTimes.Min();
+
+            for (int i = 0; i < earliestBusTimes.Count; i++)
+            {
+                if (earliestBusTimes[i] == earliestBus)
+                {
+                    return (earliestBus - earliestDepart) * buses[i];
+                }
+            }
+
+            return -1;
         }
 
         public static long Part2(List<string> data)
