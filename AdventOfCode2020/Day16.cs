9a74f5a10253c6bd026e24740a59080f419a9107
Rick Gray <rickdgray@outlook.com>
2020-12-16 23:44:44 -0600

    day 16 part 2

--- AdventOfCode2020/Day16.cs
+++ AdventOfCode2020/Day16.cs
@@ -69,7 +69,60 @@ namespace AdventOfCode2020
 
         public static long Part2(List<string> data)
         {
-            throw new NotImplementedException();
+            var rules = new Dictionary<string, string>();
+            var myTicket = new List<int>();
+            var nearbyTickets = new List<List<int>>();
+
+            var lineNumber = 0;
+            for (int i = lineNumber; i < data.Count; i++)
+            {
+                var line = data[i];
+
+                if (string.IsNullOrEmpty(line))
+                {
+                    lineNumber = i;
+                    break;
+                }
+
+                var rule = line.Split(": ");
+                rules.Add(rule[0], rule[1]);
+            }
+            lineNumber += 2;
+            myTicket.AddRange(data[lineNumber].Split(",").Select(n => int.Parse(n)).ToList());
+            lineNumber += 3;
+            for (int i = lineNumber; i < data.Count; i++)
+            {
+                var line = data[i];
+
+                if (string.IsNullOrEmpty(line))
+                {
+                    break;
+                }
+
+                nearbyTickets.Add(new List<int>());
+                nearbyTickets.Last().AddRange(data[i].Split(",").Select(n => int.Parse(n)).ToList());
+            }
+
+            var bounds = new List<Tuple<Tuple<int, int>, Tuple<int, int>>>();
+            foreach (var (_, rule) in rules)
+            {
+                var ruleTokens = rule.Split(" or ");
+                var firstRule = ruleTokens[0].Split("-");
+                var secondRule = ruleTokens[1].Split("-");
+
+                bounds.Add(Tuple.Create(Tuple.Create(int.Parse(firstRule[0]), int.Parse(firstRule[1])), Tuple.Create(int.Parse(secondRule[0]), int.Parse(secondRule[1]))));
+            }
+
+            for (int i = 0; i < nearbyTickets.Count; i++)
+            {
+                foreach (var field in nearbyTickets[i])
+                {
+                    if (bounds.All(b => field < b.Item1.Item1 || (field > b.Item1.Item2 && field < b.Item2.Item1) || field > b.Item2.Item2))
+                    {
+                        nearbyTickets.RemoveAt(i);
+                    }
+                }
+            }
         }
     }
 }
