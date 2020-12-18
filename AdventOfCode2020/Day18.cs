f073d15c10eeee9319cc8d75a7a812cfcae11c54
Rick Gray <rickdgray@outlook.com>
2020-12-18 00:12:50 -0600

    day 18

--- AdventOfCode2020/Day18.cs
+++ AdventOfCode2020/Day18.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace AdventOfCode2020
 {
@@ -7,12 +8,79 @@ namespace AdventOfCode2020
     {
         public static long Part1(List<string> data)
         {
-            throw new NotImplementedException();
+            var rootNode = new Node
+            {
+                Children = new List<Node>()
+            };
+
+            foreach (var digit in data.First())
+            {
+                switch (digit)
+                {
+                    case '(':
+                        //recurse
+                        break;
+                    case ')':
+                        //base case
+                        break;
+                    case '+':
+                    case '*':
+                        rootNode.Children.Add(new Node
+                        {
+                            Type = Type.Operation,
+                            Value = digit,
+                            Children = new List<Node>()
+                        });
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
 
         public static long Part2(List<string> data)
         {
             throw new NotImplementedException();
         }
+
+        public long RecursivelyOperate(string line, Node currentNode)
+        {
+            foreach (var digit in line)
+            {
+                switch (digit)
+                {
+                    case '(':
+                        RecursivelyOperate(line, )
+                        break;
+                    case ')':
+                        //base case
+                        break;
+                    case '+':
+                    case '*':
+                        rootNode.Children.Add(new Node
+                        {
+                            Type = Type.Operation,
+                            Value = digit,
+                            Children = new List<Node>()
+                        });
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+
+    public class Node
+    {
+        public Type Type { get; set; }
+        public char Value { get; set; }
+        public List<Node> Children { get; set; }
+    }
+
+    public enum Type
+    {
+        Number,
+        Operation
     }
 }
